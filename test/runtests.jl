19e5e7f69a4522f4cdb3e237e0e0d445806e3320
Algopaul <72927083+Algopaul@users.noreply.github.com>
2022-05-10 07:34:49 +0200

    improve test for 2D Connection

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,9 +14,12 @@ x--x  x     x--x--x
 function TestConnection_Case2D()
   Points = [[0, 0], [1, 0], [2, 0], [0, 1], [1, 1], [2, 1], [0, 2], [2, 2], [0, 3], [1, 3], [2, 3]]
   edges = [[1, 2], [1,4], [2,5], [9, 10], [10, 11]]
+  expected_edges = [[2, 3], [4, 5], [5,6], [3, 6], [4, 7], [5, 10], [6, 8], [7, 9], [8, 11]]
   M = Mesh(Points, edges)
   update_mesh(M)
-  @test length(M.edges) == 14
+  for expected_edge in expected_edges
+    @test expected_edge ∈ M.edges
+  end
 end
 
 """
